42c21da3ab3aa215c2ea522bb484c4db548396a4
Juan Jose Cordoba Paredes <juanjosec@unicauca.edu.co>
2023-06-19 17:43:07 -0500

    mover auth module a features y crear modulo subscribers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'login',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
   }
 ];
 
